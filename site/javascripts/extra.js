db609ce9bb1751b9264592d282eacf5b433f207b
Dhika Mahendra <dhikamahendra789@gmail.com>
2024-03-06 17:19:55 +0700

    remove site

--- site/javascripts/extra.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('transitionend', modifyUsingOmRoniFormatting);
-
-function modifyUsingOmRoniFormatting() {
-    document.getElementsByClassName("md-sidebar--secondary")[0].style = "--md-default-bg-color:none";
-}
